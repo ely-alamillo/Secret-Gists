982ef6242b3e2eebe8312bb1ca73324d45734698
Ely Alamillo <ely0013@gmail.com>
2017-11-10 19:04:01 -0600

    working on nacl

--- src/app.js
+++ src/app.js
@@ -1,15 +1,58 @@
 const express = require('express');
-const github = require('github');
+const Github = require('github');
+const bodyParser = require('body-parser');
+const nacl = require('tweetnacl');
+nacl.util = require('tweetnacl-util');
+const config = require('./config');
+
+const helpers = require('./helpers');
 
 const server = express();
 
-server.post('/login', (req, res) => {
-  const { username, oauth_token } = req.body;
-  // TODO log in to GitHub, return success/failure response
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+// const githubAPI = new Github({
+//   baseUri: 'https://api.github.com',
+//   token: config.GITHUB_TOKEN,
+// });
+
+const githubAPI = new Github({
+  debug: true,
 });
 
-server.get('/gists', (req, res) => {
+githubAPI.authenticate({
+  type: 'oauth',
+  token: config.GITHUB_GIST,
+});
+
+server.post('/login', (req, res) => {
+  //  const { username, oauth_token } = req.body;
+  const { username } = req.body;
+  githubAPI.users.getForUser({ username })
+    .then((response) => {
+      res.json(response.files);
+    });
+});
+
+server.post('/gists', (req, res) => {
   // TODO retrieve a list of gists for the currently authed user
+  const { username } = req.body;
+  githubAPI.gists.getForUser({ username })
+    .then((response) => {
+      res.json(response.data[0]);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+server.get('/keypair', (req, res) => {
+  const keyPair = nacl.box.keyPair();
+  const publicKey = nacl.util.encodeBase64(keyPair.publicKey);
+  const secretKey = nacl.util.encodeBase64(keyPair.secretKey);
+  res.json({ publicKey, secretKey });
 });
 
 server.listen(3000);
---
+++ src/helpers.js
@@ -0,0 +1,8 @@
+const obj2Arr = (obj) => {
+  const arr = [];
+  return Object.values(obj);
+};
+
+module.exports = {
+  obj2Arr,
+};
